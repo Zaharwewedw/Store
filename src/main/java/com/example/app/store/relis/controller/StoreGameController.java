62a4cd6432baa178c1fc6c7fb6b8eb899530c8e8
“Zahar.Write.Code” <tolst.0.2.5.0.2.5@gmail.com>
2024-02-05 22:12:03 +0300

    add new name controller game store

--- src/main/java/com/example/app/store/relis/controller/StoreGameController.java
+++ src/main/java/com/example/app/store/relis/controller/StoreGameController.java
@@ -14,32 +14,32 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/auth")
-public class AdminController {
+@RequestMapping("/store")
+public class StoreGameController {
 
     final StoreServiceImp storeService;
 
     @Autowired
-    public AdminController(StoreServiceImp storeService) {
+    public StoreGameController(StoreServiceImp storeService) {
         this.storeService = storeService;
     }
 
 
     @GetMapping("/login")
     public ModelAndView allUsers() {
-        return new ModelAndView("auth/login")
+        return new ModelAndView("/store/login")
                 .addObject("store", storeService.getListGameStoreDataBase());
     }
 
     @GetMapping("/add")
     public String addStoreGameGet(@ModelAttribute("Store") GameStore gameStore) {
-        return "auth/addGameStore";
+        return "/store/addGameStore";
     }
 
     @GetMapping("/update/{id}")
     public ModelAndView upDateStoreGame(@ModelAttribute("Store") GameStore gameStore,
                                   @PathVariable("id") Long id) {
-        return new ModelAndView("/auth/updateGameStore")
+        return new ModelAndView("/store/updateGameStore")
                 .addObject(storeService.getLastGameStoreDataBase(id));
 
     }
@@ -48,17 +48,17 @@ public class AdminController {
     public String upDateStoreGamePatch(@ModelAttribute("Store") GameStore gameStore) {
         storeService.updateGameStoreDataBase(gameStore);
 
-        return "redirect:/auth/login";
+        return "redirect:/store/login";
     }
 
     @PostMapping("/add")
     public String addStoreGame(@ModelAttribute("Store") GameStore gameStore) {
-        return storeService.saveLastGameStoreDataBase(gameStore) ? "redirect:/auth/login" : "error messege";
+        return storeService.saveLastGameStoreDataBase(gameStore) ? "redirect:/store/login" : "error messege";
     }
 
     @DeleteMapping("/delete/{id}")
     public String deleteStoreGame(@PathVariable("id") Long id) {
       storeService.deleteGameStoreDataBase(id);
-      return "redirect:/auth/login";
+      return "redirect:/store/login";
     }
 }
